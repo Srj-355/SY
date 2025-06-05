1ed12742ca70ebddc0da63d17a9ce4075b71bc72
srj-355 <saroj80977@gmail.com>
2025-06-05 10:57:52 +0545

    unnecessary comment  cleanup

--- src/main/java/com/example/sy/service/EmailService.java
+++ src/main/java/com/example/sy/service/EmailService.java
@@ -6,7 +6,6 @@ import com.example.sy.model.User;
 public interface EmailService {
     void sendPasswordResetEmail(User user, String resetToken);
     void sendAccountActivationEmail(User user);
-
     void sendBookingCancellationEmail(Booking booking);
     void sendBookingConfirmationEmail(Booking booking);
 }
